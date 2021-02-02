6b9ad05134daa2013f9d1b018115094a4f39e1f0
PatrickBoynton <jpboynton3@gmail.com>
2021-02-02 12:06:20 -0500

    Added an iffe.

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 (function() {
   'use strict';
-  
+  const BASE_URL = "https://api.github.com/users/PatrickBoynton";
+  const GITHUB_TOKEN = "";
+
+  fetch(`${BASE_URL}`, {
+    headers: {
+      Authorization: "8a0aedf8f922034f559aba3cb0b9e92e8d6339dd"
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log(data));
+
 }());
