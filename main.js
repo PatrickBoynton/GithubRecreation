4cfd127ac4d6dc9f495076a69d1b2c0c52d230af
PatrickBoynton <jpboynton3@gmail.com>
2021-02-02 13:36:27 -0500

    Added a function to display HTML.

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 (function() {
   'use strict';
   const BASE_URL = "https://api.github.com/users/PatrickBoynton";
-  const GITHUB_TOKEN = "";
+  const GITHUB_TOKEN = "8a0aedf8f922034f559aba3cb0b9e92e8d6339dd";
+
+  const generateHTML = (data) => {
+    const source = document.querySelector("#profile").innerHTML;
+    const template = Handlebars.compile(source);
+    const context = data;
+    const html = template(context);
+
+    document.querySelector(".profile-container").innerHTML = html;
+  }
 
   fetch(`${BASE_URL}`, {
     headers: {
-      Authorization: "8a0aedf8f922034f559aba3cb0b9e92e8d6339dd"
+      Authorization: GITHUB_TOKEN
     }
   })
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => generateHTML(data));
 
 }());
